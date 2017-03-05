d8e71b46e6486323307dd2efed2a2b5d92f17da6
iamtw1st <b.sydorenko@gmail.com>
2017-03-05 09:55:19 +0200

    major update of client. fixed bugs with queue and other improvements

--- client.py
+++ client.py
@@ -1,47 +1,49 @@
 #!/usr/local/bin/python3
-import socket, argparse, sys
+import socket, argparse
 
-HOST = "localhost"
+host = "localhost"
 
-p = argparse.ArgumentParser(description='Simple program for\n'
-										'sending/receiving\n'
-										'messages, with assigning\n'
+p = argparse.ArgumentParser(description='Simple program for sending/receiving messages, with assigning\n'
 										'number of queue.')
-p.add_argument('-P', '--PORT', type=int, help='Please enter valid port.')
-p.add_argument('-p', '--post', help='Enter message to\n'
-									'store/update on server, type [--queue]\n'
-									'and digit after message to assign\n'
-									'queue number')
-p.add_argument('-g', '--get', action='store_true', help='To get a specific\n'
-														'message,\n'
-														'type [--queue]\n'
-														'and enter the\n'
-														'corresponding number')
+p.add_argument('-P', '--PORT', type=int, help='Please enter valid port. port must be 0-65535')
+p.add_argument('-p', '--post', help='Enter message to store/update on server, type [--queue] and digit after message\n'
+                                    'to assignqueue number')
+p.add_argument('-g', '--get', action='store_true', help='To get a specific message, type [--queue] and enter the\n'
+                                                        'corresponding number')
 p.add_argument('-q', '--queue', help='Queue number. Can be from 0 to 10000.')
 
 
-def client(port, ge=False, po=None, qu=None):
-	con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	get = 'get' if ge else False
-	post = 'post:' + po if po else False
-	if qu and int(qu) not in range(10001):
-		p.print_help()
-		return
-	queue = ':' + qu if qu else False
-	data = ''.join([i for i in (get, post, queue) if type(i) == str])
+def client(port, g=False, po=None, q=None):
+    post = lambda x, y: bytes(x + '|||' + y, 'utf-8')
+    get = lambda x: bytes(x, 'utf-8')
 
-	if data:
-		con.connect((HOST, port))
-		con.send(bytes(data, "utf-8"))
-		r = str(con.recv(1024), "utf-8")
-		print(r)
-		con.close()
-		return r
+    if not q:
+        q = '0'
+
+    try:
+        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        if g and (int(q) in range(10001)):
+            con.connect((host, port))
+            data = get(q)
+            con.send(data)
+            recv = str(con.recv(1024), "utf-8")
+            print(recv)
+            con.close()
+            return recv
+
+        if po and (int(q) in range(10001)) and not '|||' in po:
+            con.connect((host, port))
+            data = post(po, q)
+            con.send(data)
+            recv = str(con.recv(1024), "utf-8")
+            print(recv)
+            con.close()
+            return recv
+        return p.print_help()
+    except:
+        p.print_help()
 
 
 if __name__ == '__main__':
-	if len(sys.argv[:]) < 2:
-		p.print_help()
-		exit()
-
-	client(p.parse_args().PORT, p.parse_args().get, p.parse_args().post, p.parse_args().queue)
+    client(p.parse_args().PORT, p.parse_args().get, p.parse_args().post, p.parse_args().queue)
