d4d9f8a09aed9c042597bd151cdb9d6a750d557a
iamtw1st <b.sydorenko@gmail.com>
2017-03-09 20:31:17 +0200

    final version

--- client.py
+++ client.py
@@ -1,48 +1,56 @@
 #!/usr/local/bin/python3
 import socket, argparse
 
+
 host = "localhost"
 
-p = argparse.ArgumentParser(description='Simple program for sending/receiving messages, with assigning\n'
+
+p = argparse.ArgumentParser(description='Simple program for\n'
+										'sending/receiving\n'
+										'messages, with assigning\n'
 										'number of queue.')
-p.add_argument('-P', '--PORT', type=int, help='Please enter valid port. port must be 0-65535')
-p.add_argument('-p', '--post', help='Enter message to store/update on server, type [--queue] and digit after message\n'
-                                    'to assignqueue number')
-p.add_argument('-g', '--get', action='store_true', help='Get messages')
+p.add_argument('-P', '--PORT', type=int, help='Please enter valid port. port can be from 0-65535')
+p.add_argument('-p', '--post', help='Enter message to\n'
+									'store/update on server, type [--queue]\n'
+									'and digit after message to assign\n'
+									'queue number')
+p.add_argument('-g', '--get', action='store_true', help='To get a specific\n'
+														'message,\n'
+														'type [--queue]\n'
+														'and enter the\n'
+														'corresponding number')
 p.add_argument('-q', '--queue', help='Queue number. Can be from 0 to 10000.')
 
 
 def client(port, g=False, po=None, q=None):
-    post = lambda x, y: bytes(x + '|||' + y, 'utf-8')
-    get = lambda x: bytes(x, 'utf-8')
+	
+	post = lambda x,y: bytes(x+'|||'+y, 'utf-8')
+	get = lambda x: bytes(x, 'utf-8')
 
-    if not q:
-        q = '0'
-
-    try:
-        con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        if g and (int(q) in range(10001)):
-            con.connect((host, port))
-            data = get(q)
-            con.send(data)
-            recv = str(con.recv(1024), "utf-8")
-            print(recv)
-            con.close()
-            return recv
-
-        if po and (int(q) in range(10001)) and not '|||' in po:
-            con.connect((host, port))
-            data = post(po, q)
-            con.send(data)
-            recv = str(con.recv(1024), "utf-8")
-            print(recv)
-            con.close()
-            return recv
-        return p.print_help()
-    except:
-        p.print_help()
-
-
+	if not q:
+			q = '0'	
+	try:
+		con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if g and (int(q) in range(10001)):
+			con.connect((host, port))
+			data = get(q)
+			con.send(data)
+			recv = str(con.recv(1024), "utf-8")
+			print(recv)
+			con.close()
+			return recv
+		
+		if po and (int(q) in range(10001)) and not '|||' in po:
+			con.connect((host, port))
+			data = post(po,q)
+			con.send(data)
+			recv = str(con.recv(1024), "utf-8")
+			print(recv)
+			con.close()
+			return recv
+		return p.print_help()
+	except:
+		p.print_help()
+		
 if __name__ == '__main__':
-    client(p.parse_args().PORT, p.parse_args().get, p.parse_args().post, p.parse_args().queue)
+	client(p.parse_args().PORT, p.parse_args().get, p.parse_args().post, p.parse_args().queue)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver, argparse
 
 p = argparse.ArgumentParser(description='Simple server program to send/receive messages,\n'
                                         'only 100 messages can be in queue.')
-p.add_argument('-p', '--port', type=int, help='PORT number. port must be 0-65535')
+p.add_argument('-p', '--port', type=int, help='Please enter valid port. port can be from 0-65535')
 
 
 messages = [None for i in range(10001)]
@@ -11,48 +11,53 @@ counter = 0
 
 
 class MyHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        def get(q):
-            global counter
-
-            index_m = [i for i in range(len(messages)) if messages[i]]
-            if len(index_m):
-                if int(q) in index_m:
-                    d = messages[int(q)]
-                    messages[int(q)] = None
-                    counter -= 1
-                    return bytes(d + '\n[done]', 'utf-8')
-                x = min(index_m)
-                d = messages[x]
-                messages[x] = None
-                counter -= 1
-                return bytes(d + '\n[done]', 'utf-8')
-
-        def post(q, po):
-            global counter
-            index_n = [i for i in range(len(messages)) if not messages[i]]
-            if counter < 100:
-                messages.insert(int(q), po)
-                messages.pop(max(index_n))
-                counter += 1
-                return bytes('[done]', 'utf-8')
-
-        self.data = str(self.request.recv(1024), 'utf-8').split('|||')
-        print(self.data)
-        if len(self.data) == 1:
-            self.data = get(self.data[0])
-            self.request.sendall(self.data) if self.data else None
-        if len(self.data) == 2:
-            self.data = post(self.data[1], self.data[0])
-            self.request.sendall(self.data) if self.data else None
-
-
+	def handle(self):
+		def get(q):
+			global counter
+		
+			index_m = [i for i in range(len(messages)) if messages[i]]
+			if len(index_m):
+				if int(q) in index_m:
+					d = messages[int(q)]
+					messages[int(q)] = None
+					counter -= 1
+					return bytes(d+'\n[done]', 'utf-8')
+				x = min(index_m)
+				d = messages[x]
+				messages[x] = None
+				counter -= 1
+				return bytes(d+'\n[done]', 'utf-8')
+				
+		def post(q, po):
+			global counter
+			
+			index_n = [i for i in range(len(messages)) if not messages[i]]
+			if counter<100:
+				print([i for i in messages if i])
+				if not messages[int(q)]:
+					messages[int(q)] = po
+					counter += 1
+					return bytes('[done]', 'utf-8')
+				
+				messages.insert(int(q), po)
+				messages.pop(max(index_n))
+				counter += 1
+				return bytes('[done]', 'utf-8')
+	
+		self.data = str(self.request.recv(1024), 'utf-8').split('|||')
+		
+		if len(self.data) == 1:
+			self.data = get(self.data[0])
+			self.request.sendall(self.data) if self.data else None
+		if len(self.data) == 2:
+			self.data = post(self.data[1], self.data[0])
+			self.request.sendall(self.data) if self.data else None
 if __name__ == '__main__':
-
-    if not p.parse_args().port:
-        p.print_help()
-        exit()
-    HOST, PORT = 'localhost', p.parse_args().port
-    s = socketserver.TCPServer((HOST, PORT), MyHandler)
-    print('Serving at: ', PORT)
-    s.serve_forever()
+    
+	if not p.parse_args().port:
+		p.print_help()
+		exit()
+	HOST, PORT = 'localhost', p.parse_args().port
+	s = socketserver.TCPServer((HOST, PORT), MyHandler)
+	print('serving at:', PORT)
+	s.serve_forever()
